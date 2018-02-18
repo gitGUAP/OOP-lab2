e76bfd38dbdfafce5b75be2635b088a304dead51
Grishy <grishy.ru@yandex.ru>
2018-02-18 18:49:56 +0300

    first try circle

--- lab2/circle.hpp
+++ lab2/circle.hpp
@@ -5,11 +5,55 @@
 class Circle : public virtual Shape
 {
 public:
-	Circle(uint32_t x, uint32_t y, uint32_t radius): _center(x,y), _r(radius) {}
+	Circle(uint32_t x, uint32_t y, uint32_t radius) : _x(x), _y(y), _r(radius) {}
 
 	virtual void draw(Screen* screen) const
 	{
-		screen->putPoint(_center);
+		int x1, y1, yk = 0;
+		int sigma, delta, f;
+
+		x1 = 0;
+		y1 = _r;
+		delta = 2 * (1 - _r);
+
+		do
+		{
+			screen->putPoint(_x + x1, _y + y1);
+			screen->putPoint(_x - x1, _y + y1);
+			screen->putPoint(_x + x1, _y - y1);
+			screen->putPoint(_x - x1, _y - y1);
+
+			f = 0;
+			if (y1 < yk)
+				break;
+			if (delta < 0)
+			{
+				sigma = 2 * (delta + y1) - 1;
+				if (sigma <= 0)
+				{
+					x1++;
+					delta += 2 * x1 + 1;
+					f = 1;
+				}
+			}
+			else
+				if (delta > 0)
+				{
+					sigma = 2 * (delta - x1) - 1;
+					if (sigma > 0)
+					{
+						y1--;
+						delta += 1 - 2 * y1;
+						f = 1;
+					}
+				}
+			if (!f)
+			{
+				x1++;
+				y1--;
+				delta += 2 * (x1 - y1 - 1);
+			}
+		} while (1);
 	}
 
 	virtual void move(Point p)
@@ -18,22 +62,25 @@ public:
 
 	Point getLeftTop() const
 	{
-		return Point(_center);
+		return Point(_x, _y);
 	}
 
 	Point getRightTop() const
 	{
-		return Point(_center);
+		return Point(_x, _y);
+
 	}
 
 	Point getLeftBottom() const
 	{
-		return Point(_center);
+		return Point(_x, _y);
+
 	}
 
 	Point getRightBottom() const
 	{
-		return Point(_center);
+		return Point(_x, _y);
+
 	}
 
 	virtual ~Circle()
@@ -41,6 +88,5 @@ public:
 	}
 
 protected:
-	Point _center;
-	uint32_t _r;
+	uint32_t _x, _y, _r;
 };
